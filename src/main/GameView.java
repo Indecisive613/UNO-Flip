1455cfdb96e680605d838282c75cee06517c564f
JackieSL1 <jackie@smallcity.ca>
2023-10-13 18:10:55 -0400

    Update GameView

--- src/main/GameView.java
+++ src/main/GameView.java
@@ -1,34 +1,42 @@
 package main;
 
+import java.util.ArrayList;
+
 public class GameView {
 
-    private Game game;
+    private final Game game;
 
     public GameView(Game game) {
         this.game = game;
     }
 
-    private void printStartOfTurn() {
-        System.out.printf("%s's turn.\n", "Player 1");
+    private void updateStartGame() {
+    }
+
+    public void updateNewTurn(Player player) {
+        System.out.printf("%s's Turn.\n", player.getName());
         System.out.printf("Current side: %s.\n", "Light");
-    }
-    private void printPlayerCards() {
         System.out.println("Your cards:");
-    }
 
-    private void printPlayerSelect() {
-        System.out.println("Enter number of players: ");
-    }
-
-    public void update() {
-        printStartOfTurn();
-        printPlayerCards();
+        ArrayList<Card> playersHand = player.getHand();
+        for (int i = 0; i < playersHand.size(); i++) {
+            System.out.println(i + 1 + ". " + playersHand.get(i));
+        }
+        System.out.println();
         System.out.printf("Top card %s.", "YELLOW SKIP");
     }
 
     public static void main(String[] args) {
         Game g = new Game();
+        ArrayList<Card> h = new ArrayList<>();
+        h.add(new Card(Card.Colour.BLUE, Card.Symbol.EIGHT));
+        h.add(new Card(Card.Colour.WILD, Card.Symbol.WILD_DRAW_TWO));
+        h.add(new Card(Card.Colour.BLUE, Card.Symbol.DRAW_ONE));
+        h.add(new Card(Card.Colour.RED, Card.Symbol.REVERSE));
+        Player p = new Player("Test guy 1", h);
+
+
         GameView v = new GameView(g);
-        v.update();
+        v.updateNewTurn(p);
     }
 }
