6400705c9e3fc7e613511834b2cdbbf8ba6aac77
Jake <jakesiushansian@cmail.carleton.ca>
2023-10-20 14:24:44 -0400

    Added to GameTest

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -7,11 +7,15 @@ import main.Player;
 
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.Collections;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import main.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameTest {
 
@@ -20,19 +24,19 @@ class GameTest {
     Stack<Card> deck;
     Player player1, player2,  player3, player4;
 
-    @BeforeEach
+    @Before
     public void setUp() {
-        ArrayList<Card> hand1 = new ArrayList<Card>(); // create hand for player 1
-        ArrayList<Card> hand2 = new ArrayList<Card>(); // create hand for player 2
-        ArrayList<Card> hand3 = new ArrayList<Card>(); // create hand for player 3
-        ArrayList<Card> hand4 = new ArrayList<Card>(); // create hand for player 4
+
+        ArrayList<Card> hand1 = new ArrayList<Card>(); // create empty hand for player 1
+        ArrayList<Card> hand2 = new ArrayList<Card>(); // create empty hand for player 2
+        ArrayList<Card> hand3 = new ArrayList<Card>(); // create empty hand for player 3
+        ArrayList<Card> hand4 = new ArrayList<Card>(); // empty create hand for player 4
         player1 = new Player("Homer", hand1); // create player 1
         player2 = new Player("Marge", hand2); // create player 2
         player3 = new Player("Bart", hand3); // create player 3
         player4 = new Player("Lisa", hand4); // create player 4
 
         deck = GameRunner.createDeck();
-
         game = new Game(deck); // create the game with the players and the deck
 
         game.addPlayer(player1);
@@ -43,10 +47,11 @@ class GameTest {
 
     @Test
     public void testStartGame() {
+
         assertEquals(0, player1.getHand().size());
         assertEquals(0, player2.getHand().size());
-        assertEquals(0, player4.getHand().size());
         assertEquals(0, player3.getHand().size());
+        assertEquals(0, player4.getHand().size());
         assertEquals(104, deck.size());
 
         game.dealCards();
@@ -58,5 +63,13 @@ class GameTest {
 
         // All 4 players are dealt 7 cards + 1 card placed on the table
         assertEquals(104 - 7*4 - 1, deck.size());
+
+        assertTrue(game.isRunning()); // the game should be running
+
+        // player 1 draws a card from the deck
+        game.drawCard(player1);
+        assertEquals(8, player1.getHand().size());
+        assertEquals(104 - 7*4 - 1 - 1, deck.size());
+
     }
 }
