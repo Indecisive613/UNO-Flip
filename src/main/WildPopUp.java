d6e43d706588220aa17ecd01333c7b27b5b4a298
Anand Balaram <anandbalaram@cmail.carleton.ca>
2023-10-31 22:10:09 -0400

    Implemented WildPopUp

--- src/main/WildPopUp.java
+++ src/main/WildPopUp.java
@@ -3,9 +3,16 @@ package main;
 import javax.swing.*;
 
 public class WildPopUp extends JOptionPane implements GameView {
+    Game game; // should technically create a controller to do this I guess, but it would only have 1 method...
+    public WildPopUp(JFrame parentFrame){
+        String[] strOptions = {"Red", "Blue", "Green", "Yellow"};
+        Card.Colour[] colours = {Card.Colour.RED, Card.Colour.BLUE, Card.Colour.GREEN, Card.Colour.YELLOW};
+        int choice = JOptionPane.showOptionDialog(parentFrame, "Choose one of the buttons below:", "Custom Option Pane",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, strOptions, strOptions[0]);
 
-    public WildPopUp(){
-
+        if (choice >= 0) {
+            game.setCurrentColour(colours[choice]);
+        }
     }
     public void setGame(Game game) {
 
