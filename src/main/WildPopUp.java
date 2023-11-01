6a1907ae001cb1dc2e709cb5abab2f5647b8c612
Anand Balaram <anandbalaram@cmail.carleton.ca>
2023-10-31 21:47:57 -0400

    Implemented direction for InfoViewPanel

--- src/main/WildPopUp.java
+++ src/main/WildPopUp.java
@@ -1,4 +1,45 @@
 package main;
 
-public class WildPopUp {
+import javax.swing.*;
+
+public class WildPopUp extends JOptionPane implements GameView {
+
+    public WildPopUp(){
+
+    }
+    public void setGame(Game game) {
+
+    }
+
+    public void updateGetPlayerName(int index) {
+
+    }
+
+    public void updateInvalidInput(String message) {
+
+    }
+
+    public void handleNewTurn(Player player) {
+
+    }
+
+    public void updatePlayCard(Card playedCard, String additionalMessage) {
+
+    }
+
+    public void updateDrawCard(Card drawnCard) {
+
+    }
+
+    public void updateGetCard() {
+
+    }
+
+    public void updateGetColour() {
+
+    }
+
+    public void updateConfirmColour(Card.Colour colour) {
+
+    }
 }
