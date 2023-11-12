582447e98e9bb260c387f8647ebdf45a8137f489
Anand Balaram <anandbalaram@cmail.carleton.ca>
2023-11-11 22:38:48 -0500

    More UML and restored method that was removed

--- src/main/GameView.java
+++ src/main/GameView.java
@@ -44,6 +44,11 @@ public interface GameView {
     void handleDrawCard(Card drawnCard);
 
     /**
+     * Display instructions to the current player about what colour to pick
+     */
+    void handleGetColour();
+
+    /**
      * Update the status of the turn order for the players whenever a reverse card is played
      *
      * @param turnReversed whether the turn order of the players has been reversed
